92603386be3020cf9c8b33b31cfae0f85cbae36c
anatawa12 <anatawa12@icloud.com>
2020-04-24 12:56:23 +0900

    couldn't use eid

--- src/main/java/com/anatawa12/entitydebugger/EntityIdSelector.kt
+++ src/main/java/com/anatawa12/entitydebugger/EntityIdSelector.kt
@@ -1,12 +1,17 @@
 package com.anatawa12.entitydebugger
 
 import com.google.common.base.Predicate
+import net.minecraft.command.EntitySelector
 import net.minecraft.command.ICommandSender
 import net.minecraft.entity.Entity
 import net.minecraft.util.math.Vec3d
 import net.minecraftforge.fml.common.IEntitySelectorFactory
 
 class EntityIdSelector : IEntitySelectorFactory {
+    init {
+        EntitySelector.addArgument("eid")
+    }
+
     override fun createPredicates(arguments: MutableMap<String, String>, mainSelector: String?, sender: ICommandSender?, position: Vec3d?): List<Predicate<Entity>> {
         val eid = arguments["eid"]?.toIntOrNull() ?: return listOf()
         return listOf(Predicate {
